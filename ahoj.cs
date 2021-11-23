498c0ad2026cd0806a6cf5b0d20c8a428e508063
IQcrew <itiqcrew@gmail.com>
2021-11-23 17:47:37 +0100

    updatik
    idk

--- ahoj.cs
+++ ahoj.cs
@@ -6,7 +6,8 @@ namespace ahoj
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");    
+      Console.WriteLine("Hello World!"); 
+      Systsyem.Consosle.WritefLine(); 
     }
   }
 }
