cda04e63f82e51981fd2fae8f92594531db38e97
PeterGonscak <91431312+PeterGonscak@users.noreply.github.com>
2021-11-25 18:06:31 +0100

    commit 0.3

--- UltraFighters/Assets/scripts/player/Player.cs
+++ UltraFighters/Assets/scripts/player/Player.cs
@@ -4,26 +4,83 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float MovementX;
-    Rigidbody2D myBody;
-    [SerializeField]
-    private float Speed = 5;
-    bool isGrounded;
+    public float MoveForce = 10;
+    public float JumpForce = 3;     // Premenne na nastavenie pohybu - aby som sa nemusel furt chodit tam a spet po dokonceni fixnut a dat private
+
+    public KeyCode Right;          // Nastavovanie pre klavesy pomocou unity
+    public KeyCode Left;
+    public KeyCode Up;
+    public KeyCode Down;
+    public KeyCode Slot1;
+    public KeyCode Slot2;
+    public KeyCode Slot3;
+
+    private Vector2 Movement;
+
+    bool isGrounded = false;
+    bool Jumped = false;
+
+    Rigidbody2D PlayerBody;
+
     // Start is called before the first frame update
     void Start()
     {
+        PlayerBody = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        MovementX = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(MovementX, 0f, 0f) * Time.deltaTime * Speed;
-    }
-    void Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
-            myBody.AddForce(new Vector2(0f, 10), ForceMode2D.Impulse);
-    }
+        if (isGrounded)
+        {
+            if (Input.GetKey(Right))
+            {
+                PlayerBody.velocity = new Vector2(MoveForce, PlayerBody.velocity.y);
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (Input.GetKey(Left))
+            {
+                 PlayerBody.velocity = new Vector2(-MoveForce, PlayerBody.velocity.y);
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+                PlayerBody.velocity = new Vector2(0f, PlayerBody.velocity.y);
+        }
+        else if (Jumped)
+        {
+            if (Input.GetKey(Right))
+            {
+                PlayerBody.AddForce(new Vector2(MoveForce/200, 0f), ForceMode2D.Impulse);
+                Jumped = true;
+            }   
+            else if (Input.GetKey(Left))
+            {
+                PlayerBody.AddForce(new Vector2(-MoveForce/200, 0f), ForceMode2D.Impulse);
+                Jumped = true;
+            }
+        }
 
+        if (Input.GetKey(Up) && isGrounded)
+        {
+            PlayerBody.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+             
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            Jumped = false;
+        }    
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            Jumped = true;
+        }
+    }
 }
