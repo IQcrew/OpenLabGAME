0d6fb8f1063688b3d1e5f1481cf3aa48ae068c78
IQcrew <itiqcrew@gmail.com>
2021-12-29 21:25:13 +0100

    ---GunSpawn---

--- UltraFighters/Assets/scripts/map/GunSpawner.cs
+++ UltraFighters/Assets/scripts/map/GunSpawner.cs
@@ -8,41 +8,34 @@ public class GunSpawner : MonoBehaviour
     public Sprite MedKit;
     private Gun ActualItem;
     private bool IsMedKit = false;
-    private int LengtOfDatabase;
     private int Timer= 0;
     private float StartTime;
-    private Sprite ObjTexture;
     private int TempRandom;
-    private void Start(){
-        ObjTexture = ThisGameObject.GetComponent<SpriteRenderer>().sprite;
-        Setup(); }
+    private float TickTime = 0f;
+    private void Start(){ Setup(); }
     private void Update()
     {
-        Debug.Log(ActualItem.GunTexture.ToString()+"  "+ObjTexture.ToString()+"  "+Timer.ToString()+ "  "+ Time.time.ToString(name));
-        if (StartTime + Timer > Time.time) {}
+
+        if (StartTime + Timer > Time.time) {}  //waiting
         else if (StartTime + Timer + 1 > Time.time)
         {
-            if (IsMedKit) { ObjTexture = MedKit; }
-            else { ObjTexture = ActualItem.GunTexture; }
-            ThisGameObject.SetActive(true);
+            if (IsMedKit) { ThisGameObject.GetComponent<SpriteRenderer>().sprite = MedKit; }
+            else { ThisGameObject.GetComponent<SpriteRenderer>().sprite = ActualItem.GunTexture; }
         }
         else if (StartTime + Timer + 15 > Time.time)
         {
-
         }
         else if (StartTime + Timer + 25 > Time.time)
         {
-            if (int.Parse(StartTime.ToString()) % 2 == 1)
+            if (Time.time-TickTime < 0.5f)
             {
-                if (IsMedKit) { ObjTexture = MedKit; }
-                else { ObjTexture = ActualItem.GunTexture; }
+                if (IsMedKit) { ThisGameObject.GetComponent<SpriteRenderer>().sprite = MedKit; }
+                else { ThisGameObject.GetComponent<SpriteRenderer>().sprite = ActualItem.GunTexture; }
             }
-            else
-            {
-                ObjTexture = null;
-            }
+            else if (Time.time - TickTime < 1f) { ThisGameObject.GetComponent<SpriteRenderer>().sprite = null;}
+            else{TickTime = Time.time; }
         }
-        else { Setup(); }
+        else {Setup(); }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -71,8 +64,7 @@ public class GunSpawner : MonoBehaviour
     }
     private void Setup()
     {
-        ThisGameObject.SetActive(false);
-        ObjTexture = null;
+        ThisGameObject.GetComponent<SpriteRenderer>().sprite = null;
         Timer = Random.Range(5, 30);
         StartTime = Time.time;
         TempRandom = Random.Range(0,1100);
@@ -82,9 +74,9 @@ public class GunSpawner : MonoBehaviour
             if (TempRandom < 400) { ActualItem = GetGun("Pistol"); }
             else if (TempRandom < 550) { ActualItem = GetGun("Eagle"); }
             else if (TempRandom < 700) { ActualItem = GetGun("Mac-10"); }
-            else if (TempRandom < 800) { ActualItem = GetGun("Pistol"); }
+            else if (TempRandom < 800) { ActualItem = GetGun("Eagle"); }
             else if (TempRandom < 900) { ActualItem = GetGun("SniperRifle"); }
-            else if (TempRandom < 950) { ActualItem = GetGun("SniperRifle"); }
+            else if (TempRandom < 950) { ActualItem = GetGun("AssalutRifle"); }
             else if (TempRandom < 1100) { ActualItem = GetGun("Shotgun"); }
         }
     }
