e6cfe90a4deb8a3d4b706df2e7365c207e5861ca
IQcrew <itiqcrew@gmail.com>
2022-01-21 15:58:12 +0100

    ---AudioUpdate---

--- UltraFighters/Assets/scripts/map/objectHP.cs
+++ UltraFighters/Assets/scripts/map/objectHP.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class objectHP : MonoBehaviour
 {
     public int health = 80;
-    public GameObject HitEffect;
-    public GameObject DeathEffect;
-
-
+    public AudioClip HitSound;
+    public AudioClip DeathSound;
+    private AudioSource AudioManagerSet;
+    private void Start()
+    {
+        AudioManagerSet = GameObject.Find("AudioManager").GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     public void TakeDamage (int damage)
@@ -17,12 +20,12 @@ public class objectHP : MonoBehaviour
 
         if (health <= 1)     //DeathEffect and remove object
         {
-
+            AudioManagerSet.PlayOneShot(DeathSound);
             Destroy(gameObject);
         }
         else      //HitEffect
-        {     
-            
+        {
+            AudioManagerSet.PlayOneShot(HitSound);
         }
     }
 }
