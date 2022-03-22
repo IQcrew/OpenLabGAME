98405e995aa56cb202ebb1a2497e56a93322caa3
IQcrew <94800323+IQcrew@users.noreply.github.com>
2022-03-22 09:43:37 +0100

    createdDataManager

---
+++ UltraFighters/Assets/scripts/level/dataManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class dataManager
+{
+    private static string gamePath = "assets/data/game.json";
+    private static string settingsPath = "assets/data/settings.json";
+    
+    static dataManager()
+    {
+        
+        checkFiles();
+    }
+    public static void updateData()
+    {
+
+    }
+    public static void checkFiles()
+    {
+
+    }
+}
