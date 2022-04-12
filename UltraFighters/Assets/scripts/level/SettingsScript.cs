ef179230bc3c948ad9819eb1237f4814a3bc3739
IQcrew <94800323+IQcrew@users.noreply.github.com>
2022-04-12 19:43:18 +0200

    buttonColor

--- UltraFighters/Assets/scripts/level/SettingsScript.cs
+++ UltraFighters/Assets/scripts/level/SettingsScript.cs
@@ -14,6 +14,7 @@ public class SettingsScript : MonoBehaviour
     [SerializeField] private AudioClip clickSound;
     [SerializeField] private GameObject KeyBindsButtons;
     [SerializeField] private TextMeshProUGUI warningBox;
+    [SerializeField] private Sprite Image;
     void Start()
     {
         dataManager.readAllData();
